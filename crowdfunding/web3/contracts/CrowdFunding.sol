99a012cc2e824636b05b358f33c312502cb271f3
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-27 16:44:57 +0300

    done smart contract

--- crowdfunding/web3/contracts/CrowdFunding.sol
+++ crowdfunding/web3/contracts/CrowdFunding.sol
@@ -62,8 +62,15 @@ contract CrowdFunding {
 
     function getCampaigns() public view returns (Campaign[] memory){
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
         
     }
-
 
 }
